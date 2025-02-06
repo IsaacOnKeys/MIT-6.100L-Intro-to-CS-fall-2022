f68a4be78e8c00a200db43b44a519ab20bd13d5a
Isaac <isaaconkeys@protonmail.com>
2025-02-06 17:23:52 +0100

    complete finger exercise 9

--- Lecture 2/youtryit2.py
+++
@@ -1,8 +0,0 @@
-############## YOU TRY IT ###############
-# Buggy, fix it!
-x = int(input("Enter a number for x: "))
-y = int(input("Enter a different number for y: "))
-if x == y:
-    print("These are equal!")
-else:
-	print(x,"is not the same as",y)
